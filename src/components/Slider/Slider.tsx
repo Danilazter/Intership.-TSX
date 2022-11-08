5973424f836e81e08f7bc44ab17cb41342e38d2f
Danilaz <idralton@gmail.com>
2022-11-08 23:49:21 +0300

    Slider continue

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -4,13 +4,27 @@ import styles from "./Slider.module.css";
 import { SliderProps } from './Slider.props';
 
 
+const FADE_DURATION = 300;
 
 export const Slider = ({ reviews }: SliderProps) => {
     const [slide, setSlide] = useState<number>(0);
+    const [fadeState, setFadeState] = useState<'fade-in' | 'fade-out'>('fade-in');
+    const [currentTimer, setCurrentTimer] = useState<NodeJS.Timeout>();
+
+    const handlerClick = (move: number) => {
+        const timer = setTimeout(() => { 
+            setSlide(s => s + move);
+            setFadeState('fade-in')
+        }, FADE_DURATION);
+        clearTimeout(currentTimer)
+        setFadeState('fade-out')
+            setCurrentTimer(timer)
+    }
+
     return (
         <div className={styles.slider}>
-            <div className={styles.slide}
-                style={{ backgroundImage: `url(${reviews[slide].image})` }}
+            <div className={styles.slide} 
+                style={{ backgroundImage: `url(${reviews[slide].image})`, transitionDuration: `${FADE_DURATION}ms` }}
                 
             >
             <div className={styles.textBlock}>
@@ -19,12 +33,12 @@ export const Slider = ({ reviews }: SliderProps) => {
                     <button className={styles.slideButton}>Подробнее</button>
                 </div>
                 <div className={styles.sliderButtons}>
-                <button className={styles.previous}>
-                <Arrow className={styles.Arrow} />
-            </button>
-            <button className={styles.next}>
-                <Arrow className={styles.Arrow} />
-                    </button>
+                    {slide > 0 && <button className={styles.previous} onClick={() => handlerClick(-1)}>
+                        <Arrow className={styles.Arrow} />
+                    </button>}
+                    {slide < reviews.length - 1 && <button className={styles.next} onClick={() => handlerClick(+1)}>
+                        <Arrow className={styles.Arrow} />
+                    </button>}
                     </div>
             </div>
             
